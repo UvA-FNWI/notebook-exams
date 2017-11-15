65bd864fa01a9825195811890b1bbf3595aff7ef
Jesse van der Sar <jesse@sar.nl>
2017-11-15 17:01:31 +0100

    Scripts voor student-folders en student-blaadjes toegevoegd

--- Snippets/Theorie-vragen in notebooks/questions.py
+++ Snippets/Theorie-vragen in notebooks/questions.py
@@ -2,6 +2,9 @@ from ipywidgets import widgets
 from IPython.display import display, Markdown, Latex
 from IPython.core.display import HTML
 
+import os.path
+import json
+
 display(HTML("""<style>
 h4 {
     margin-top: 20px;
@@ -13,20 +16,51 @@ h4 {
 </style>"""))
 
 def create_input(q):
-    return widgets.Text(
-        placeholder='Vul in...'
+    w = widgets.Text(
+        placeholder='Vul in...',
+        value=answers[q]
     )
+    
+    w.question = q
+    w.observe(answer_changed)
+    
+    return w
+
+def answer_changed(change):
+    if change['name'] == 'value':
+        q = change.owner.question
+        answers[q] = change.new
+        
+        save_answers()
+        
+def save_answers():
+    with open(answer_file, 'w') as f:
+        json.dump(answers, f)
+    
+answer_file = 'answers.json'
 
 questions = [
     'Wat is het kwadraat van 8?',
     'Wie is de docent van dit vak?'
 ]
 
+if os.path.isfile('answers.json'):
+    with open(answer_file) as f:
+        answers = json.load(f)
+else:
+    answers = { q: '' for q in questions }
+    
+    save_answers()
+    
 fields = [ (q, create_input(q)) for q in questions ]
 
 def get_answers():
-	return { question: field.value for (question, field) in fields }
+	return answers
 
+def pretty_print_answers():
+    for i, (q, a) in enumerate(answers.items()):
+        display(Markdown('#### '+ str(i + 1) +'. '+ q))
+        display(Markdown('*'+ a +'*'))
 
 def ask():
 	for i, (question, field) in enumerate(fields):
