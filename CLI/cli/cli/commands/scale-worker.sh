efc8e59c7f66f81b14f1c16f85d6b929a2a186a5
Jesse van der Sar <jesse@sar.nl>
2017-12-21 12:36:22 +0100

    A lot of updates

--- CLI/cli/cli/commands/scale-worker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcloud compute instances stop $1
-gcloud compute instances set-machine-type $1 --custom-cpu $2 --custom-memory $3
-gcloud compute instances start $1
-docker-machine regenerate-certs --force $1
